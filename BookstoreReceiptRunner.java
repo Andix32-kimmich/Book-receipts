7b362040f21e474d7cef010652647a18a223fd8d
Andix32-kimmich <andixtorrealba2408@gmail.com>
2026-05-20 11:12:37 -0400

    Add files via upload

---
+++ BookstoreReceiptRunner.java
@@ -0,0 +1,15 @@
+public class BookstoreReceiptRunner
+{
+    public static void main(String[] args)
+    {
+        //Create 2 BookstoreReceipt objects
+     BookstoreReceipt receive1 = new BookstoreReceipt ("Bob Clinton", 5, "THANK YOU!");         
+     BookstoreReceipt receipt2 = new BookstoreReceipt("Sarah Johnson", 2, "ENJOY YOUR PUCHASE!");
+     
+     
+     //Print out the receipts
+        System.out.println(receipt1.getReceipt();
+        System.out.println();
+        System.out.println(receipt2.getReceipt();
+    }
+}
